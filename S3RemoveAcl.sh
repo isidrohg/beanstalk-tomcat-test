22d46a33b8a2b10a9880581f767c37de4129f800
Isidro Hernandez <isidrohg@viniza.tech>
2018-09-21 14:34:37 -0700

    committing

--- S3RemoveAcl.sh
+++ S3RemoveAcl.sh
@@ -11,11 +11,6 @@ do
   s3key1=$(aws s3api list-objects --bucket elasticbeanstalk-us-west-2-957862771990 --prefix "resources/environments/e-qxacvw7vpn/_runtime/_versions/Tomcat8Apache" --query 'Contents[0].Key' --output text)
   s3key2=$(aws s3api list-objects --bucket elasticbeanstalk-us-west-2-957862771990 --prefix "resources/environments/e-qxacvw7vpn/_runtime/_versions/Tomcat8Apache" --query 'Contents[1].Key' --output text)
 
-  #echo $s3key1
-  #echo $s3key2
-  #if "$s3key1" -ne "None"; then
-  #  originalAcl1=$(aws s3api get-object-acl --bucket elasticbeanstalk-us-west-2-957862771990 --key "${s3key1}")
-  #fi
   originalAcl1=$(aws s3api get-object-acl --bucket elasticbeanstalk-us-west-2-957862771990 --key "${s3key1}")
   if [ $s3key2 != $emptyKey ]; then
     originalAcl2=$(aws s3api get-object-acl --bucket elasticbeanstalk-us-west-2-957862771990 --key "${s3key2}")
@@ -41,14 +36,6 @@ do
     echo "`date`: not found, continuing..."
   fi
 
-  sleep 1
-  #echo "S3 Key"
-  #echo $s3key
-  #echo "Original Acl"
-  #echo $originalAcl
-  #echo "PutObjectAcl Response"
-  #echo $putObjectAclResponse
-  #echo "New Acl"
-  #echo $newAcl
+  sleep 0.5
 
 done
